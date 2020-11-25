6eb693f80f098968d353a80ada9189e3df46239a
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-25 10:24:26 +0900

    1125途中

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -81,9 +81,17 @@ class TodoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(CreateTodoRequest  $request, $id)
     {
-        //
+        //フォームから送信されたタスクを書き換える
+        $todo = Auth::yser()->todos()->findOrFail($id);
+        $todo->title = $request->title;
+        $todo->due_data = $request->due_data;
+        $todo->save();
+
+        //タスク単体のページにリダイレクト
+        return redirect()->to('/todo/' . $todo->id);
+
     }
 
     /**
